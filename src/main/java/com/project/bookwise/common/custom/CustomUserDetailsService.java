93dedf1c40fb10912d2efe3e15b46bc8b110a268
lwer210 <lwer210@naver.com>
2025-04-25 21:17:24 +0900

    feature: UserDetailsService 구현체 추가

---
+++ src/main/java/com/project/bookwise/common/custom/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.project.bookwise.common.custom;
+
+import com.project.bookwise.user.domain.User;
+import com.project.bookwise.user.mapper.UserMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserMapper userMapper;
+
+    public CustomUserDetailsService(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userMapper.getUserInfoByUserId(username);
+        return new CustomUserDetails(user);
+    }
+}
