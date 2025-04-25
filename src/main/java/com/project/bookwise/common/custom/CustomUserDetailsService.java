11277c034f3487022138277327c77883d1dadba9
lwer210 <lwer210@naver.com>
2025-04-26 07:57:54 +0900

    feature: UserDetailsService 구현체 Component 어노테이션 적용

--- src/main/java/com/project/bookwise/common/custom/CustomUserDetailsService.java
+++ src/main/java/com/project/bookwise/common/custom/CustomUserDetailsService.java
@@ -5,7 +5,9 @@ import com.project.bookwise.user.mapper.UserMapper;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserMapper userMapper;
